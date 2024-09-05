6603db7b3b2215907b75838ab5b0a847fcc35e69
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-09-05 22:48:20 +1000

    check CLI exists implemented. check CLI arg is int implemented

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() 
+// convert the key to integer
+// request plaintext
+// check for upper and lower case
+// apply formula
+// output cipher
+
+int main(int argc, char *argv[])
 {
-    
+    // check if command line argument exists
+    if (argc == 2)
+    {
+        int key = atoi(argv[1]);
+        bool valid_key = false;
+
+        for (int i = 0; i < strlen(argv[1]); i++)
+        {
+            // check if command line argument is int
+            if (isdigit(argv[1][i]))
+            {
+                valid_key = true;
+            }
+            else
+            {
+                printf("Usage: ./vigenere-cipher <key>\n");
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        printf("Usage: ./vigenere-cipher <key>\n");
+        return 1;
+    }
 }
